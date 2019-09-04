898ac2d6d7dd01c0b79d30bfe95024fe7b339fa4
suheldiverse <53937618+suheldiverse@users.noreply.github.com>
2019-09-04 18:42:00 +0530

    Update processData.js

--- api/controllers/processData.js
+++ api/controllers/processData.js
@@ -6,12 +6,22 @@ const MongoClient = require('mongodb').MongoClient;
 //const redisPort=5000;
 
 const express = require('express');
+var bodyparser =  require('body-parser');
 const responseTime = require('response-time')
 const axios = require('axios');
 const redis = require('redis');
+var cors = require('cors');
 const app = express();
 const client = redis.createClient();
 app.use(responseTime());
+
+
+
+app.use(cors({origin:"http://localhost:3000"}));
+app.use(cors({origin:"http://localhost:5000"}));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended : false}));
 
 
 let url=global.gConfig.url;  
@@ -45,7 +55,10 @@ exports.firstAPIData=function(req,res){
 
 
 exports.secondAPIData=function(req,res){
+
+        //././data/data.csv
         const query = 1; //(req.query.query).trim();
+        let timeInterval=15; // 15 mintus time interval
         // Try fetching the result from Redis first in case we have it cached
         return client.get(`myDataFirstApi:${query}`, (err, result) => {
                 if (err) return res.json(err);
@@ -58,19 +71,55 @@ exports.secondAPIData=function(req,res){
                         console.log('Data is not available in Redis');
                 // Key does not exist in Redis store
                 // Fetch directly from Mongodb  API
-
                         MongoClient.connect(mongoURL, { useNewUrlParser: true }, function(err, db) {
-                                var dbo = db.db(databaseName);
-                                dbo.collection("trade").find().limit(5).toArray(function(err, result) {
+                                var dbo = db.db(databaseName);  
+                                var arr = []; 
+                                dbo.collection("trade").find({"timestamp":{ $gte:"2019-07-16T12:18:02.458Z", $lt:"2019-09-04T12:18:04.397Z" }
+                        }).limit(100000).toArray(function(err, result) {
+                                var cnt=result.length;
+                                var volume=0;
+                                for (var i=0;i<cnt;i++) {
+                                     let cDateTime= result[i]['timestamp'];
+                                     let currentDate = Date.parse(cDateTime);
+                                      
+                                     if(i==0){
+                                        var endDate   = Date.parse(cDateTime)+timeInterval;
+                                        var startDate = Date.parse(cDateTime);
+                                        var arrayData=[];                                       
+                                     }
+                                    
+                                    if(currentDate<=endDate){
+                                        volume=volume+result[i]['size'];
+                                        arrayData.push(result[i]['price']);                                                                               
+                                     }else{
+                                        var high  = Math.max(...arrayData);
+                                        var low   = Math.min(...arrayData);
+                                        var open  = arrayData[0];
+                                        var close = arrayData.pop();
+
+                                        arr.push({
+                                                date:   startDate,
+                                                open:   open,
+                                                high:   high,
+                                                low:    low,                                               
+                                                close:  close,
+                                                volume: volume                                               
+                                              });                                              
+                                        
+                                        var endDate   = Date.parse(cDateTime)+timeInterval;
+                                        var startDate = Date.parse(cDateTime);
+                                        var arrayData=[];
+                                        var volume=0;
+                                        volume=volume+result[i]['size'];
+                                        arrayData.push(result[i]['price']);
+                                     }
+                                  } // end for loop
                                 if (err) return res.json(err);
-                                client.setex(`myDataFirstApi:${query}`, 60, JSON.stringify({ source: 'Redis Cache', ...result, }));
+                                client.setex(`myDataFirstApi:${query}`, 10, JSON.stringify({ source: 'Redis Cache', ...arr, }));
                                 db.close();
-                                return res.status(200).json({ source: 'Mongo db', ...result, });                    
+                                return res.status(200).json({ source: 'Mongo db', ...arr, });                    
                                 });    
-                        });       
+                        });  // end mongo client      
                 }
         });
-}
-
-    
-       
+}       
